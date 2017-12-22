f8234a2b833f3d1c942cadf00f1fe3ab1c8cde75
abcdevelop <bruno.panaget@yahoo.fr>
2017-12-22 09:06:04 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <pp-header></pp-header>
+    </div>
+    <div class="container">
+      <pp-menu></pp-menu>
+    </div>
+    <div class="container">
+      <pp-footer></pp-footer>
+    </div>
+  </div>
+</template>
+
+<script>
+  import ppHeader from './components/Header.vue';
+  import ppHome from './components/Home.vue';
+  import ppFooter from './components/Footer.vue';
+  import ppMenu from './components/Menu.vue';
+
+  export default {
+    components:{
+      ppHeader,
+      ppHome,
+      ppFooter,
+      ppMenu
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="row">
+    <nav class="navbar navbar-toggleable-md navbar-light">
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/">home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">about us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">admin</a>
+        </li>
+      </ul>
+    </nav>
+  </footer>
+</template>
